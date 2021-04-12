d69025faa8d6d9e128b3819c5eaa71faadae3596
Tereza Burianova <40072706+T-ess@users.noreply.github.com>
2021-04-13 00:15:11 +0200

    Checks + initial instructions before jumps

--- interpret.py
+++ interpret.py
@@ -19,6 +19,31 @@ ERR_VALUE_MISSING = 56
 ERR_VALUE_WRONG = 57
 ERR_STRING = 58
 
+def err(msg, code):
+    print(msg, file=sys.stderr)
+    sys.exit(code)
+
+def value_validity(attr_type, text):
+    text_regex = {
+        'var' : r'^(GF|LF|TF)@[a-žA-Ž_\-$&%*!?][a-žA-Ž0-9_\-$&%*!?]*$',
+        'label' : r'^[a-žA-Ž_\-$&%*!?][a-žA-Ž0-9_\-$&%*!?]*$',
+        'type' : r'^(int|string|bool)$',
+        'int' : r'^[\-]?[0-9]+$',
+        'bool' : r'^(true|false)$',
+        'string' : r'^([^\s#\\\\]|\\[0-9]{3})*$',
+        'nil' : r'^nil$'
+    }
+    if attr_type == 'string' and text is None:
+        text = ''
+    text.replace("\\\\", "\\")
+    try:
+        if (re.search(re.compile(text_regex[attr_type]), text) is None):
+            return False
+    except:
+        err("Argument text is missing.", ERR_INVALID_STRUCT)
+    return True
+
+
 class preparation:
     int_source = sys.stdin
     int_input = sys.stdin
@@ -56,17 +81,20 @@ class preparation:
         args = parser.parse_args()
         if not (args.source or args.input):
             parser.error('Add -source or -input. See --help for more info.')
-        elif (args.source):
+        elif args.source:
             self.int_source = args.source
         else:
-            self.int_input = args.int_input
+            self.int_input = args.input
 
     def xml_parse(self):
-        tree = ET.parse(self.int_source)
-        self.root = tree.getroot()
+        try:
+            tree = ET.parse(self.int_source)
+            self.root = tree.getroot()
+        except:
+            err("Invalid XML format.", ERR_INVALID_FORMAT)
         #* check needed before sorting
         for child in list(self.root):
-            if (child.tag != 'instruction'):
+            if child.tag != 'instruction':
                 err("Invalid XML structure: 'instruction' expected.", ERR_INVALID_STRUCT)
         #* sort instructions based on "order" attribute
         for child in self.root.iter():
@@ -74,17 +102,17 @@ class preparation:
 
     def xml_validity(self):
         #* check program tag validity
-        if (self.root.tag != 'program'):
+        if self.root.tag != 'program':
             err("The 'program' tag is missing.", ERR_INVALID_STRUCT)
         if (not all(item in ['language', 'name', 'description'] for item in self.root.attrib)):
             err("Invalid attributes in 'program'.", ERR_INVALID_STRUCT)
-        if (('language' not in self.root.attrib) or (self.root.attrib['language'] != 'IPPcode21')):
+        if ('language' not in self.root.attrib) or (self.root.attrib['language'] != 'IPPcode21'):
             err("Attribute 'language' in 'program missing or invalid.", ERR_INVALID_STRUCT)
         #* check instruction tag validity
         for child in self.root.findall('instruction'):
             if (not all(item in ['opcode', 'order'] for item in child.attrib)):
                 err(f"Invalid attributes in 'instruction'.", ERR_INVALID_STRUCT)
-            if ('opcode' not in child.attrib or 'order' not in child.attrib):
+            if ('opcode' not in child.attrib) or ('order' not in child.attrib):
                 err("Missing 'instruction' attribute 'order' or 'opcode'.", ERR_INVALID_STRUCT)
             opcode = child.get("opcode").upper()
             if opcode not in self.instruction_dict:
@@ -93,42 +121,119 @@ class preparation:
             #* check instruction childern (args)
             argnum = 1
             for arg in list(child):
-                if (arg.tag != ('arg' + str(argnum))): #* invalid tag name in args
+                if arg.tag != ('arg' + str(argnum)): #* invalid tag name in args
                     err("Invalid tags.", ERR_INVALID_STRUCT)
-                if (len(arg.attrib) != 1 or ('type' not in arg.attrib)):
+                if (len(arg.attrib) != 1) or ('type' not in arg.attrib):
                     err("Invalid 'arg' attributes.", ERR_INVALID_STRUCT)
                 type_attr = arg.get('type')
                 valid_type_attr = ['int', 'bool', 'string', 'nil', 'label', 'type', 'var']
-                if (type_attr == None or type_attr not in valid_type_attr): #* invalid type attribute in arg tags
+                if (type_attr is None) or (type_attr not in valid_type_attr): #* invalid type attribute in arg tags
                     err("Invalid 'arg' attributes.", ERR_INVALID_STRUCT)
                 #* check text validity
-                self.text_validity(type_attr, arg.text)
+                text_valid = value_validity(type_attr, arg.text)
+                if not text_valid:
+                    err("Invalid text inside an argument.", ERR_INVALID_STRUCT)
                 #* change type attribute in case of symb
-                if (type_attr in ['string', 'int', 'nil', 'bool']):
+                if type_attr in ['string', 'int', 'nil', 'bool']:
                     type_attr = 'symb'
-                if (type_attr == 'var' and self.instruction_dict[opcode][argnum-1] == 'symb'):
-                    type_attr = 'symb'
+                try:
+                    if type_attr == 'var' and self.instruction_dict[opcode][argnum-1] == 'symb':
+                        type_attr = 'symb'
+                except:
+                    err("Invalid arguments.", ERR_INVALID_STRUCT)
                 args_current.append(type_attr)
                 argnum += 1
-            if (args_current != self.instruction_dict[opcode.upper()]):
+            if args_current != self.instruction_dict[opcode.upper()]:
                 err("Invalid 'instruction' arguments.", ERR_INVALID_STRUCT)
 
-    def text_validity(self, attr_type, text):
-        text_regex = {
-            'var' : r'^(GF|LF|TF)@[a-žA-Ž_\-$&%*!?][a-žA-Ž0-9_\-$&%*!?]*$',
-            'label' : r'^[a-žA-Ž_\-$&%*!?][a-žA-Ž0-9_\-$&%*!?]*$',
-            'type' : r'^(int|string|bool)$',
-            'int' : r'^[\-]?[0-9]+$',
-            'bool' : r'^(true|false)$',
-            'string' : r'^([^\s#\\\\]|\\\\[0-9]{3})*$',
-            'nil' : r'^nil$'
-        }
-        if (re.search(re.compile(text_regex[attr_type]), text) == None):
-            err("Invalid argument text.", ERR_INVALID_STRUCT)
+class frame:
+    def __init__(self):
+        self.variables = {}
+
+    def define_variable(self, var_name):
+        if var_name in self.variables:
+            err(f"Variable '{var_name}' is already defined.", ERR_SEM)
+        self.variables[var_name] = None
+
+    def edit_variable(self, var_name, var_value, var_type):
+        if var_name not in self.variables:
+            err(f"Variable '{var_name}' does not exist.", ERR_VAR)
+        if self.variables[var_name] is None:
+            self.variables[var_name] = [var_type, var_value]
+        else:
+            if var_type != self.variables[var_name][0]:
+                err(f"Inavlid value type: variable is of type {self.variables[var_name][0]}.", ERR_SEM)
+            self.variables[var_name] = [var_type, var_value]
+
+class labels:
+    labels_storage = {}
+
+    def __init__(self, xml_root):
+        for instruction in xml_root.findall('instruction'):
+            if instruction.get('opcode') == 'LABEL':
+                label_name = instruction[0].text
+                if label_name in self.labels_storage:
+                    err(f"Label {label_name} already exists.", ERR_SEM)
+                self.labels_storage[label_name] = instruction.get('order')
+
+class interpret:
+    GF = None
+    TF = None
+    LF_stack = []
+    LF = None
+    stack_call = []
+
+    def __init__(self):
+        prep = preparation()
+        self.GF = frame()    
+        label = labels(prep.root)
+        call_stack = []
+        # instruction_list = prep.root.findall('instruction')
+        for instruction in prep.root.findall('instruction'):
+            self.call_instruction(instruction.get('opcode').upper(), instruction)
+
+    def call_instruction(self, name: str, instr):
+        if hasattr(self, name) and callable(instr_method := getattr(self, name)):
+            instr_method(instr)
+    
+    def check_frame(self, frame_type):
+        if frame_type == 'LF':
+            if self.LF_stack:
+                self.LF = self.LF_stack[-1]
+            else:
+                err("Frame does not exist.", ERR_FRAME)
+        elif frame_type == 'TF':
+            if self.TF is None:
+                err("Frame does not exist.", ERR_FRAME)
+
+    def MOVE(self, instr):
+        pass
+
+    def CREATEFRAME(self, instr):
+        self.TF = frame()
+    
+    def PUSHFRAME(self, instr):
+        if self.TF is None:
+            err("Temporary frame is not defined.", ERR_FRAME)
+        self.LF_stack.append(self.TF)
+        self.TF = None
+    
+    def POPFRAME(self, instr):
+        if not self.LF_stack:
+            err("Local frame stack is empty.", ERR_FRAME)
+        self.TF = self.LF_stack.pop()
+
+    def DEFVAR(self, instr):
+        var = instr[0].text.split('@', 1)
+        self.check_frame(var[0])
+        current_frame = getattr(self, var[0])
+        current_frame.define_variable(var[1])
+        
+    def CALL(self, instr):
+        
+
+    def RETURN(self, instr):
 
 
-def err(msg, code):
-    print(msg, file=sys.stderr)
-    sys.exit(code)
 
-preparation()
+interpret()
